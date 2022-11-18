9bef9746c5eea03ee313c01f4a0000367efcff19
TheusFreecs <matheus1509950@gmail.com>
2022-11-17 21:04:12 -0300

    oi

---
+++ calculadora-simples.js
@@ -0,0 +1,26 @@
+function soma (x,y,resultadoSoma){
+     resultadoSoma = 0;
+     return resultadoSoma =  x + y;
+}
+
+
+function divisao (x,y,resultadoDivisao){
+    resultadoDivisao = 0;
+    return resultadoDivisao = x / y;
+}
+
+function multiplicacao (x,y,resultadoMultiplicacao){
+    resultadoMultiplicacao = 0;
+    return resultadoMultiplicacao =  x * y;
+}
+
+function subtracao (x,y,resultadoSubtracao){
+    resultadoSubtracao =  0;
+    return resultadoSubtracao = x - y; 
+}
+
+
+console.log(soma(15,15));
+console.log(divisao(6,2));
+console.log(multiplicacao(6,2));
+console.log(subtracao(6,2));
